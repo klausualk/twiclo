2cebab149015bdc29eefaf5e34755f39f323e9bf
Klaussualk <klausjogi@gmail.com>
2015-08-28 17:23:58 +0300

    esimene push

---
+++ twitterClone.js
@@ -0,0 +1,44 @@
+Tweets = new Meteor.Collection("tweets");  
+if (Meteor.isClient) {
+
+Template.tweetBox.onRendered(function () {  
+  Session.set('numChars', 0);
+});
+
+
+Template.tweetBox.events({  
+  'input #tweetText': function(){
+    Session.set('numChars', $('#tweetText').val().length);
+  },
+  'click button': function() {  
+  var tweet = $('#tweetText').val();
+  $('#tweetText').val("");
+  Session.set('numChars', 0);
+  Tweets.insert({message: tweet});
+}
+});
+
+Template.tweetBox.helpers({  
+  charCount: function() {
+    return 140 - Session.get('numChars');
+  },
+
+  charClass: function() {
+    if (Session.get('numChars') > 140) {
+      return 'errCharCount';    //css class name
+    } else {
+      return 'charCount';       //css class name
+    }
+  },
+
+  disableButton: function() {
+    if (Session.get('numChars') <= 0 ||
+        Session.get('numChars') > 140) {
+      return 'disabled';
+    }
+  }
+});
+
+  
+}
+
